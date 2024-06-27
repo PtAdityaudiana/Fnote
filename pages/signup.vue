b6f31957428e9b66cc1a367340bee52a9f37bfe1
Aditya-Udiana <adityaudiana16@gmail.com>
2024-06-27 22:47:48 +0800

    update bg color

--- pages/signup.vue
+++ pages/signup.vue
@@ -1,8 +1,18 @@
 <template>
     <div class="min-h-screen flex items-center justify-center bg-gradient-to-tr from-gray-400 via-gray-600 to-blue-800">
-      <div class="max-w-lg w-full bg-gray-300 p-8 rounded-lg shadow-lg">
+      <div class="max-w-lg w-full bg-gradient-to-r from-rose-100 to-teal-100 p-8 rounded-lg shadow-lg">
         <h2 class="text-2xl font-bold text-center mb-6">Signup</h2>
         <form @submit.prevent="signup">
+          
+          <div class="mb-4">
+            <label for="username" class="block text-gray-700">Username</label>
+            <input
+              type="text"
+              id="username"
+              v-model="username"
+              class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring focus:border-blue-300"
+            />
+          </div>
           <div class="mb-4">
             <label for="email" class="block text-gray-700">Email</label>
             <input
@@ -35,7 +45,7 @@
             type="submit"
             class="w-full py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-700 transition duration-300 mt-3"
           >
-            Signup
+            Create
           </button>
         </form>
       </div>
@@ -46,12 +56,14 @@
   import { ref } from 'vue'
   
   const email = ref('')
+  const username = ref('')
   const password = ref('')
   const confirmPassword = ref('')
   
   const signup = () => {
     if (password.value === confirmPassword.value) {
       console.log('Email:', email.value)
+      console.log('Username:', username.value)
       console.log('Password:', password.value)
     } else {
       console.error('Passwords do not match')
